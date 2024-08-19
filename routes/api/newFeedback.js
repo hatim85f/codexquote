866bdfbb23af30bc3364f9bddc577dc978421ee5
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-08-20 02:10:10 +0400

    adding the final stage for client feedback

--- routes/api/newFeedback.js
+++ routes/api/newFeedback.js
@@ -31,7 +31,9 @@ router.post("/", async (req, res) => {
     });
 
     await newFeedback.save();
-    return res.status(200).send("Thank you for submitting your feedback!");
+    return res
+      .status(200)
+      .send({ message: "Thank you for submitting your feedback!" });
   } catch (error) {
     return res.status(500).send({
       error: "Server error",
